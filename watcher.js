51d36be67216f1bd48e88d53d6e32ff582340263
Ed Mckain <mckain.default@gmail.com>
2013-12-26 18:36:01 -0600

    enable Command-line arguments using the process global object.

--- watcher.js
+++ watcher.js
@@ -1,8 +1,15 @@
 // NodeJS file watcher script.
 // Dec 26, 2013 ~ 6:20 PM
 
-const fs = require('fs');
-fs.watch('files/target_a.txt', function() {
-  console.log("The file 'target_a.txt' just changed! :3");
+const 
+  fs = require('fs');
+  filename = process.argv[2];
+
+if (!filename) {
+  throw Error('A file to watch must be specified in the params.');
+}
+
+fs.watch(filename, function() {
+  console.log("The file '" + filename + "' just changed! :3");
 });
-console.log('Node is now watching target_a.txt for changes ...');
+console.log('Node is now watching' + filename+ ' for changes ...');
