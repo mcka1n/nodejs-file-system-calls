ecf005e60e47751822e78b9ba92fa2ff1eff06b0
Ed Mckain <mckain.default@gmail.com>
2013-12-26 18:18:50 -0600

    add a script description inside watcher.js

--- watcher.js
+++ watcher.js
@@ -1,3 +1,6 @@
+# NodeJS file watcher script.
+# Dec 26, 2013 ~ 6:20 PM
+
 const fs = require('fs');
 fs.watch('files/target_a.txt', function() {
   console.log("The file 'target_a.txt' just changed! :3");
