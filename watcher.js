4842c90ac48f994a353e0ed0158c06ec42ec6b17
Ed Mckain <mckain.default@gmail.com>
2013-12-26 18:10:39 -0600

    add console log call in order to know if it is running.

--- watcher.js
+++ watcher.js
@@ -2,3 +2,4 @@ const fs = require('fs');
 fs.watch('files/target_a.txt', function() {
   console.log("The file 'target_a.txt' just changed! :3");
 });
+console.log('Node is now watching target_a.txt for changes ...');
